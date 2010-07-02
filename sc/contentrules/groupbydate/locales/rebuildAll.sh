22374d0b5705ee78e002f22bc94c25245b331f4f
Erico Andrei <erico@simplesconsultoria.com.br>
2010-07-02 03:00:18 +0000

    Updates i18n for sc.contentrules.groupbydate

--- sc/contentrules/groupbydate/locales/rebuildAll.sh
+++ sc/contentrules/groupbydate/locales/rebuildAll.sh
@@ -9,3 +9,8 @@ i18ndude rebuild-pot --pot sc.contentrules.groupbydate.pot --create sc.contentru
 # finally, update the po files
 i18ndude sync --pot sc.contentrules.groupbydate.pot  `find . -iregex '.*\.po$'|grep -v plone`
 
+for lang in $(find . -mindepth 1 -maxdepth 1 -type d); do
+    if test -d $lang/LC_MESSAGES; then
+        msgfmt -o $lang/LC_MESSAGES/sc.contentrules.groupbydate.mo $lang/LC_MESSAGES/sc.contentrules.groupbydate.po
+    fi
+done
