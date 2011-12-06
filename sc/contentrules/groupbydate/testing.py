880a1a3b2a3231d92bc1e5d27e6c44de2cf36bbf
Érico Andrei <erico@simplesconsultoria.com.br>
2011-12-06 00:30:30 -0200

    Load CMFPlacefulWorkflow

--- sc/contentrules/groupbydate/testing.py
+++ sc/contentrules/groupbydate/testing.py
@@ -10,28 +10,31 @@ from plone.app.testing import FunctionalTesting
 
 class Fixture(PloneSandboxLayer):
 
-    defaultBases = (PLONE_FIXTURE,)
+    defaultBases = (PLONE_FIXTURE, )
 
     def setUpZope(self, app, configurationContext):
         # Load ZCML
         import sc.contentrules.groupbydate
         self.loadZCML(package=sc.contentrules.groupbydate)
+        import Products.CMFPlacefulWorkflow
+        self.loadZCML(package=Products.CMFPlacefulWorkflow)
         z2.installProduct(app, 'Products.CMFPlacefulWorkflow')
 
     def setUpPloneSite(self, portal):
         # Install into Plone site using portal_setup
-        self.applyProfile(portal, 'sc.contentrules.groupbydate:default')
         wf = getattr(portal, 'portal_workflow')
         types = ('Document', 'Folder', 'Link', 'Topic')
         wf.setChainForPortalTypes(types, 'simple_publication_workflow')
-
+        self.applyProfile(portal, 'sc.contentrules.groupbydate:default')
+        self.applyProfile(portal,
+                          'Products.CMFPlacefulWorkflow:CMFPlacefulWorkflow')
 
 FIXTURE = Fixture()
 INTEGRATION_TESTING = IntegrationTesting(
-    bases=(FIXTURE,),
+    bases=(FIXTURE, ),
     name='sc.contentrules.groupbydate:Integration',
     )
 FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(FIXTURE,),
+    bases=(FIXTURE, ),
     name='sc.contentrules.groupbydate:Functional',
     )
