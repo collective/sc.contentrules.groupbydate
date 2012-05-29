8de94f14457996b1d558dd866ef429a1f4da5755
Alejandro Pereira <alepereira86@gmail.com>
2012-05-28 22:49:37 -0300

    adding user can select the content type used as container option. Issue #3

--- sc/contentrules/groupbydate/interfaces.py
+++ sc/contentrules/groupbydate/interfaces.py
@@ -2,24 +2,24 @@
 
 from zope.interface import implements
 from zope.interface import Interface
+
 from zope.schema import Choice
-from zope.schema import Text
-from zope.schema import Bool
 from zope.schema import TextLine
 
-from zope.schema.interfaces import ISource, IContextSourceBinder
+from zope.schema.interfaces import IContextSourceBinder
 
 from sc.contentrules.groupbydate.vocabulary import RelPathSearchableTextSource as SearchableTextSource
 
 from sc.contentrules.groupbydate import MessageFactory as _
 
+
 class SearchableTS(object):
     implements(IContextSourceBinder)
-    
+
     def __init__(self, query, default_query=None):
         self.query = query
         self.default_query = default_query
-    
+
     def __call__(self, context):
         context = getattr(context, 'context', context)
         results = SearchableTextSource(context, base_query=self.query.copy(),
@@ -28,18 +28,22 @@ class SearchableTS(object):
 
 
 class IGroupByDateAction(Interface):
-    """Configuration available for this content rule
+    """ Configuration available for this content rule
     """
     base_folder = Choice(title=_(u"Base folder"),
                         description=_(u"Choose the base folder for the date hierarchy."),
                         required=True,
-                        source=SearchableTS({'is_folderish' : True},
+                        source=SearchableTS({'is_folderish': True},
                                              default_query='path:')
                         )
-    
+
+    container = Choice(title=u"Container",
+                       source='sc.contentrules.groupbydate.vocabulary.containers',
+                       default=('folder', 'Folder'),
+                       required=False)
+
     structure = TextLine(title=_(u"Hierarchy structure"),
                       description=_(u"Choose hierarchy structure. Use strftime formating. e.g.: '%Y/%m/%d' to have 2011/11/17 or '%Y/%m' to have 2011/11"),
                       required=True,
                       default=u'%Y/%m/%d',
                       )
-    
--- sc/contentrules/groupbydate/vocabulary.py
+++ sc/contentrules/groupbydate/vocabulary.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import itertools
+import logging
+
 from zope.interface import implements
 
 try:
@@ -8,16 +9,21 @@ except ImportError:
     from zope.app.schema.vocabulary import IVocabularyFactory
 
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from Products.CMFCore.utils import getToolByName
+
+from Products.CMFCore.interfaces._content import IFolderish
+
 from plone.app.vocabularies.catalog import SearchableTextSource
 from plone.app.vocabularies.catalog import QuerySearchableTextSourceView
+
 from plone.app.vocabularies.terms import BrowsableTerm
 
-from zope.app.form.browser.interfaces import ISourceQueryView, ITerms
-
-from sc.contentrules.groupbydate.config import RELPATHVOC
-from sc.contentrules.groupbydate.config import STRUCTURES
+from sc.contentrules.groupbydate.config import RELPATHVOC, STRUCTURES
 
 from sc.contentrules.groupbydate import MessageFactory as _
+
+logger = logging.getLogger('sc.contentrules.groupbydate')
 
 
 class HierarchiesVocabulary(object):
@@ -40,6 +46,7 @@ class HierarchiesVocabulary(object):
 
 HierarchiesVocabularyFactory = HierarchiesVocabulary()
 
+
 class RelPathSearchableTextSource(SearchableTextSource):
     """ A special case of a SearchableTextSource where we always support
         relative paths
@@ -48,34 +55,68 @@ class RelPathSearchableTextSource(SearchableTextSource):
         """Return whether the value is available in this source
         """
         if not (value[0] == '.'):
-            result = super(RelPathSearchableTextSource,self).__contains__(value)
+            result = super(RelPathSearchableTextSource,
+                           self).__contains__(value)
         else:
             result = True
         return result
-    
+
     def search(self, query_string):
         """ Add relative paths to vocabulary
         """
-        results = super(RelPathSearchableTextSource,self).search(query_string)
+        results = super(RelPathSearchableTextSource,
+                        self).search(query_string)
         relPaths = RELPATHVOC.keys()
         results = relPaths + list(results)
         return (r for r in results)
-    
+
 
 class RelPathQSTSourceView(QuerySearchableTextSourceView):
-    """ A special case of a QuerySearchableTextSourceView where we always support
-        relative paths
+    """ A special case of a QuerySearchableTextSourceView where we
+        always support relative paths
     """
     def getTerm(self, value):
         if not (value[0] == '.'):
-            return super(RelPathQSTSourceView,self).getTerm(value)
+            return super(RelPathQSTSourceView, self).getTerm(value)
         terms = RELPATHVOC
         token = value
-        title = terms.get(value,value)
+        title = terms.get(value, value)
         browse_token = parent_token = None
-        return BrowsableTerm(value, token=token, title=title, 
-                                 description=value,
-                                 browse_token=browse_token,
-                                 parent_token=parent_token)
-        
-    
+        return BrowsableTerm(value, token=token, title=title,
+                             description=value,
+                             browse_token=browse_token,
+                             parent_token=parent_token)
+
+
+class ContainerSearcher(object):
+    """
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        context = getattr(context, 'context', context)
+        portal_url = getToolByName(context, 'portal_url')
+        site = portal_url.getPortalObject()
+        new_list = []
+        pt = getToolByName(site, 'portal_types')
+        types = pt.listTypeInfo()
+        for site_type in types:
+            if 'item' in site.keys():
+                del site['item']
+
+            try:
+                item = site_type._constructInstance(site, 'item')
+            except (TypeError, ValueError):
+                item = None
+                logger.info('There is a problem to see if %s is Folderish' %
+                            site_type)
+                pass
+
+            if item:
+                if IFolderish.providedBy(item):
+                    new_list.append(SimpleTerm(site_type.getId().lower(),
+                                    site_type.getId()))
+                del site['item']
+        return SimpleVocabulary(new_list)
+
+ContainerSearcherFactory = ContainerSearcher()
