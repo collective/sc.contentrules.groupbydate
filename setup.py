498aaee28efecaa55056ca69de25a6f1b7cd401f
hvelarde <hector.velarde@gmail.com>
2012-07-03 15:18:27 -0300

    pinned versions for Plone 4.0 compatibility

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(name='sc.contentrules.groupbydate',
       zip_safe=False,
       install_requires=[
         'setuptools',
-        'plone.directives.form==1.0',
+        'plone.directives.form==1.0',  # XXX: why?
         ],
       extras_require={
         'test': ['plone.app.testing'],
