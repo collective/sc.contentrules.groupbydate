9f13d3b5c5615c5d661c0e841923f6ac8d45d099
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-31 20:33:21 -0200

    Remove old Five code

--- sc/contentrules/groupbydate/__init__.py
+++ sc/contentrules/groupbydate/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding=utf-8 -*-
-#
 from zope.i18nmessageid import MessageFactory as BaseMessageFactory
-MessageFactory = BaseMessageFactory('sc.contentrules.groupbydate')
 
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
+MessageFactory = BaseMessageFactory('sc.contentrules.groupbydate')
