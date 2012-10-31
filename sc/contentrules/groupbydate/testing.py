217e053beac8020fd009c494e9cc6b95004dffa8
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-31 20:37:58 -0200

    Simplify testing (no need to deal with CMFPlacefulWorkflow)

--- sc/contentrules/groupbydate/testing.py
+++ sc/contentrules/groupbydate/testing.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from plone.testing import z2
-
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
@@ -16,18 +14,9 @@ class Fixture(PloneSandboxLayer):
         # Load ZCML
         import sc.contentrules.groupbydate
         self.loadZCML(package=sc.contentrules.groupbydate)
-        import Products.CMFPlacefulWorkflow
-        self.loadZCML(package=Products.CMFPlacefulWorkflow)
-        z2.installProduct(app, 'Products.CMFPlacefulWorkflow')
 
     def setUpPloneSite(self, portal):
-        # Install into Plone site using portal_setup
-        wf = getattr(portal, 'portal_workflow')
-        types = ('Document', 'Folder', 'Link', 'Topic')
-        wf.setChainForPortalTypes(types, 'simple_publication_workflow')
-        self.applyProfile(portal, 'sc.contentrules.groupbydate:default')
-        self.applyProfile(portal,
-                          'Products.CMFPlacefulWorkflow:CMFPlacefulWorkflow')
+        pass
 
 FIXTURE = Fixture()
 INTEGRATION_TESTING = IntegrationTesting(
