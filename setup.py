8ccb715be00cc0c031f1a489283a984a02cde5dd
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-31 20:46:11 -0200

    PEP8

--- setup.py
+++ setup.py
@@ -1,35 +1,31 @@
 # -*- coding:utf-8 -*-
-
 import os
 from setuptools import setup, find_packages
 
-version = '1.3b5.dev0'
-long_description = open("README.txt").read() + "\n" + \
-                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
-                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
-                   open(os.path.join("docs", "HISTORY.txt")).read()
+version = '2.0.dev0'
+long_description = (open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
+                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read())
 
 setup(name='sc.contentrules.groupbydate',
       version=version,
-      description="A Plone content rules action that creates a chronological archive.",
+      description=u"A Plone content rules action that creates a chronological "
+                  u"archive.",
       long_description=long_description,
       classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 3.3",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Framework :: Plone :: 4.2",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='plone contentrules actions',
+          "Development Status :: 5 - Production/Stable",
+           "Environment :: Web Environment",
+           "Framework :: Plone",
+           "Framework :: Plone :: 4.2",
+           "Intended Audience :: System Administrators",
+           "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+           "Operating System :: OS Independent",
+           "Programming Language :: Python",
+           "Programming Language :: Python :: 2.7",
+           "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='plone contentrules actions date grouping',
       author='Simples Consultoria',
       author_email='products@simplesconsultoria.com.br',
       url='http://www.simplesconsultoria.com.br/',
@@ -39,8 +35,8 @@ setup(name='sc.contentrules.groupbydate',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'setuptools',
-        ],
+          'setuptools',
+      ],
       extras_require={
         'test': ['plone.app.testing'],
         },
