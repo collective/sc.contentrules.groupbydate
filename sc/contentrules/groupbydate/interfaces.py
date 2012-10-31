48fba112bcc15cb9a135494bc326d79c8f5c1ba8
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-31 20:36:34 -0200

    Remove default_view handling

--- sc/contentrules/groupbydate/interfaces.py
+++ sc/contentrules/groupbydate/interfaces.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from zope.interface import implements
-from zope.interface import invariant
 from zope.interface import Interface
-from zope.interface import Invalid
 
 from zope.schema import Choice
 from zope.schema import TextLine
@@ -45,30 +43,12 @@ class IGroupByDateAction(Interface):
                        description=_(u"Select the type of container in which the structure will be based."),
                        source='sc.contentrules.groupbydate.vocabulary.containers',
                        default='Folder',
-                       required=True,
-                       )
-
-    default_view = TextLine(title=_(u"Default View"),
-                      description=_(u"Default view that container will be render."),
-                      default=u'folder_listing',
-                      required=True,
-                      )
+                       required=True)
 
     structure = TextLine(title=_(u"Hierarchy structure"),
-                      description=_(u"Choose hierarchy structure. Use strftime formating. e.g.: '%Y/%m/%d' to have 2011/11/17 or '%Y/%m' to have 2011/11"),
-                      required=True,
-                      default=u'%Y/%m/%d',
-                      )
-
-    @invariant
-    def validate_view(rule_form):
-        """ A validator to check if the view is available for that
-            container
-        """
-        portal = getSite()
-        pt = portal.portal_types
-        container = rule_form.container
-        view = rule_form.default_view
-        factory = pt[container]
-        if not view in factory.view_methods:
-            raise ViewFail
+                         description=_(u"Choose hierarchy structure. Use "
+                                       u"strftime formating. e.g.: '%Y/%m/%d'"
+                                       u" to have 2011/11/17 or '%Y/%m' to "
+                                       u"have 2011/11"),
+                         required=True,
+                         default=u'%Y/%m/%d')
