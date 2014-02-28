28af47f32aac1cdc2c5d16967ac72bcaddaf01ef
Érico Andrei <erico@simplesconsultoria.com.br>
2014-02-28 17:09:44 -0300

    Remove unused GLOBALS

--- src/sc/contentrules/groupbydate/config.py
+++ src/sc/contentrules/groupbydate/config.py
@@ -1,8 +1,8 @@
 # -*- coding=utf-8 -*-
 from sc.contentrules.groupbydate import MessageFactory as _
 
+
 PROJECTNAME = "sc.contentrules.groupbydate"
-GLOBALS = globals()
 
 RELPATHVOC = {'../': _(u" One level up"),
               './': _(u' Same folder of content')}
